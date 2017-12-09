411643bd85b705eb11e1574be2e0b92e0e0e6c9b
Liz Burton <liz.burton147@gmail.com>
2017-12-09 19:29:56 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,6 +65,6 @@ class Owner
     self.pets.each do | pet_type, pets_of_type |
       pet_counts[pet_type] = pets_of_type.count
     end
-    "I have #{fishes.count} fish, #{cats.count} cats(s), and #{dogs.count} dog(s)."
+    "I have #{pet_counts[fishes]} fish, #{pet_counts[cats]} cats(s), and #{pet_counts[dogs]} dog(s)."
   end
 end
