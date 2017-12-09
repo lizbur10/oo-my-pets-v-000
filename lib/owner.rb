d7ca96593c4ed9bf17be04014b1c6bdd9b5ab3ca
Liz Burton <liz.burton147@gmail.com>
2017-12-09 18:28:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,12 @@
 class Owner
+  attr_reader :species
+  @@all = []
 
-def initialize(species)
-  @species = species
-  @pets = {fishes: [], cats: [], dogs: [] }
-end
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: [] }
+  end
+
+  
 
 end
